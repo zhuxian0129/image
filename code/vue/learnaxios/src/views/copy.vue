80c22f4ed974a215c2d15ab823d2adcc9ced87b2
zhuxian0129 <zhuxian@soosupply.com>
2022-09-21 17:31:13 +0800

    123

---
+++ code/vue/learnaxios/src/views/copy.vue
@@ -0,0 +1,65 @@
+export default [
+  { name: '系统管理', label: '1' },
+  { name: '权限管理', path: '/permission' },
+  { name: '财务科目管理', path: '/finance' },
+  { name: '部门管理', path: '/department' },
+  { name: '人员管理', path: '/people' },
+  {
+    name: '业务收支名目',
+    submenu: [
+      { name: '业务收入名目', path: '/business/income' },
+      { name: '业务支出名目', path: '/business/expenditure' }
+    ]
+  },
+  { name: '产品线管理', path: '/productLine' },
+  { name: '客户管理', path: '/client' },
+  { name: '供应商管理', path: '/supply' },
+  { name: '数据管理', label: '1' },
+  { name: '费用管理', path: '/data/marg' },
+  { name: '产品线收支管理', path: '/data/pred' },
+  { name: '报表统计', label: '1' },
+  {
+    name: '费用统计',
+    submenu: [
+      { name: '统计', path: '/statement/cost' },
+      { name: '按科目', path: '/statement/cost/dept' },
+      { name: '按部门', path: '/statement/cost/finance' },
+      { name: '按供应商', path: '/statement/cost/supply' }
+    ]
+  },
+  {
+    name: '业务统计',
+    submenu: [
+      { name: '统计', path: '/statement/business' },
+      { name: '按产品线', path: '/statement/business/productLine' },
+      { name: '按部门', path: '/statement/business/dept' },
+      { name: '按供应商', path: '/statement/business/supply' },
+      { name: '按客户', path: '/statement/business/client' }
+    ]
+  },
+  { name: '平衡毛利率', path: '/statement/balance' },
+  { name: '事务管理', label: '4' },
+  { name: '项目管理', path: '/projects' },
+  {
+    name: '绩效管理',
+    submenu: [
+      { name: '指标维度', path: '/achievements/dimension' },
+      { name: '指标下达', path: '/achievements/release' },
+      { name: '绩效挂钩', path: '/achievements/ahook' }
+    ]
+  },
+  { name: '内务管理', path: '/internal' },
+  {
+    name: '文件上传',
+    submenu: [
+      { name: '查阅', path: '/upload' },
+      { name: '历史上传', path: '/upload/history' }
+    ]
+  },
+  { name: 'SKU设置', label: '5' },
+  { name: '已有SKU列表', path: '/sku' },
+  { name: '添加SKU', path: '/addSKU' },
+  { name: '商品设置', label: '6' },
+  { name: '已有商品列表', path: '/product' },
+  { name: '添加商品', path: '/addProduct' }
+]
--- markdown/vue/key属性的作用.md
+++ markdown/vue/key属性的作用.md
@@ -1,8 +1,8 @@
 ### 官方解释
 
-- 在Vue的<font color='#f00'>虚拟DOM算法</font>，在<font color='#f00'>新旧nodes</font>对比时辨识VNodes；
-- 如果不使用key，Vue会使用一种最大限度减少动态元素并且尽可能的尝试就地修改/复用相同类型元素的算法；
-- 而使用key，它会基于key的变化重新排列元素顺序，并且销毁key不存在的元素，创建新增的key；
+- 在Vue的<font color='#f00'>虚拟DOM算法</font>，在<font color='#f00'>新旧nodes</font>对比时辨识<font color='#f00'>VNodes</font>；
+- 如果<font color='#f00'>不使用key</font>，Vue会使用一种最大限度减少动态元素并且尽可能的尝试就地<font color='#f00'>修改/复用相同类型元素</font>的算法；
+- 而<font color='#f00'>使用key</font>，它会基于key的变化<font color='#f00'>重新排列元素顺序</font>，并且<font color='#f00'>销毁key不存在的元素</font>，<font color='#f00'>创建新增的key</font>；
 
 ### VNode -- Virtual Node
 
@@ -50,4 +50,27 @@ template -- VNode -- 真实DOM
 ```
 
 <img src='../../img/202209182151486.png'>
+
+### diff算法
+
+diff算法作用：对比新旧节点数据，更新DOM
+
+> 存在key
+> 使用patchKeyedChildren方法
+>
+> 记录新旧节点的一些变量：c1 新VNodes节点数组；c2 旧VNodes节点数组；i = 0 记录头部遍历下标；l2 旧节点长度；e1 新节点尾部下标；e2 旧节点尾部下标
+>
+> 1. 从头部开始遍历，依次对比各项，更新 i，直到遇到不同的两个节点，跳出while循环
+> 2. 从尾部开始遍历，依次对比各项，更新e1、e2，直至遇到两个不同的节点，跳出while循环
+> 3. 常规序列（i > e1 或者 i > e2）新增节点 / 移除多余节点
+> 4. 非常规序列，循环旧的节点，建立新旧节点映射，尝试匹配新的节点，并删除不再存在的节点，最后移动和挂载节点
+
+> 没有key
+>
+> 使用patchUnkeyedChildren方法
+>
+> 1. 获取新旧节点nodes数组长度
+> 2. 取两数最小值，遍历nodes，进行patch
+> 3. 如果旧nodes长度较大，移除剩余旧节点
+> 4. 反之，新增新节点
 
